c52d444148c1f658b444b97bb8b85b1414de7b6b
panugothrakesh <rakeshpanugoth2003@gmail.com>
2024-10-24 02:35:09 +0530

    Fixes: Added lazy load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,43 +2,48 @@
 
 import Navbar from '@/components/Navbar/navbar';
 import Hero from '@/components/Hero/hero';
-import Meet from '@/components/Meet/Meet';
-import Numbers from '@/components/Numbers/numbers';
 import { motion } from "framer-motion";
-import Defi_Unlocked from '@/components/Defi/Defi_Unlocked';
-import GHO from '@/components/GHO/gho';
-import Aave_Everywhere from '@/components/Aave_Everywhere/Aave_Everywhere';
-import Trusted_By from '@/components/Trusted_By/Trusted_by';
-import Governed_Card from '@/components/Governed_Card/Governed_Card';
-import Security from '@/components/Security/Security';
-import Faqs from '@/components/FAQs/Faqs';
-import Footer from '@/components/Footer/Footer';
+import { Suspense, lazy } from 'react';
+
+const Meet = lazy(() => import('@/components/Meet/Meet'));
+const Numbers = lazy(() => import('@/components/Numbers/numbers'));
+const Defi_Unlocked = lazy(() => import('@/components/Defi/Defi_Unlocked'));
+const GHO = lazy(() => import('@/components/GHO/gho'));
+const Aave_Everywhere = lazy(() => import('@/components/Aave_Everywhere/Aave_Everywhere'));
+const Trusted_By = lazy(() => import('@/components/Trusted_By/Trusted_by'));
+const Governed_Card = lazy(() => import('@/components/Governed_Card/Governed_Card'));
+const Security = lazy(() => import('@/components/Security/Security'));
+const Faqs = lazy(() => import('@/components/FAQs/Faqs'));
+const Footer = lazy(() => import('@/components/Footer/Footer'));
 
 export default function Home() {
 
-  const pageload={
-    hidden:{opacity:0},
-    visible:{opacity:1,
-      transition:{duration:0.4, ease: 'easeIn', once:true }
+  const pageload = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.4, ease: 'easeIn', once: true }
     },
   }
 
   return (
     <div>
-        <Navbar />
-        <motion.main initial='hidden' animate='visible' variants={pageload} className='h-full min-h-[100dvh]'>
-          <Hero/>
-          <Meet/>
-          <Numbers/>
-          <Defi_Unlocked/>
-          <GHO/>
-          <Aave_Everywhere/>
-          <Trusted_By/>
-          <Governed_Card/>
-          <Security/>
-          <Faqs/>
-          <Footer/>
-        </motion.main>
+      <Navbar />
+      <motion.main initial='hidden' animate='visible' variants={pageload} className='h-full min-h-[100dvh]'>
+        <Hero />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Meet />
+          <Numbers />
+          <Defi_Unlocked />
+          <GHO />
+          <Aave_Everywhere />
+          <Trusted_By />
+          <Governed_Card />
+          <Security />
+          <Faqs />
+          <Footer />
+        </Suspense>
+      </motion.main>
     </div>
   );
 }
